fd57cf23169323932b870bb67178fb091fe46f61
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-14 18:07:42 -0500

    Listado de usuarios

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { UsersService } from '@services/users.service';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent, RouterModule],
   templateUrl: './users.component.html',
   styles: ``
 })
 export default class UsersComponent {
+
+  public usersService = inject( UsersService )
+
 
 }
