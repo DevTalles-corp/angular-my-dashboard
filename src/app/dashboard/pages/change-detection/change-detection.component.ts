b157c837a928ce24cfbad585dba0177fc7b02df1
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-14 15:56:10 -0500

    ChangeDetection implementado

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, TitleComponent],
+  template: `
+
+    <app-title [title]="currentFramework()" />
+
+
+    <pre> {{ frameworkAsSignal() | json }} </pre>
+    <pre> {{ frameworkAsProperty | json }} </pre>
+
+
+  `
 })
 export default class ChangeDetectionComponent {
+
+
+  public currentFramework = computed(
+    () => `Change detection - ${ this.frameworkAsSignal().name }`
+  );
+
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+
+  constructor() {
+
+    setTimeout(() => {
+
+
+        // this.frameworkAsProperty.name = 'React';
+        this.frameworkAsSignal.update( value => {
+          value.name = 'React';
+
+          return {...value};
+        })
+
+        console.log('Hecho');
+    }, 3000);
+
+
+  }
+
 
 }
