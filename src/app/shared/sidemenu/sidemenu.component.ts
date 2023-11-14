77593c5c2b01c3c1e0f357ec1335e8342e10220f
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-14 14:43:34 -0500

    Sidemenu implementado

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { routes } from '../../app.routes';
+
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
-  styleUrl: './sidemenu.component.css'
+  styleUrl: './sidemenu.component.css',
 })
 export class SidemenuComponent {
 
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
+
+  constructor() {
+    // const dashboardRoutes = routes
+    //   .map( route => route.children ?? [] )
+    //   .flat()
+    //   .filter( route => route && route.path )
+    //   .filter( route => !route.path?.includes(':') )
+    // console.log(dashboardRoutes);
+  }
 }
